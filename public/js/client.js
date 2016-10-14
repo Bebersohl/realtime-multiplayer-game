922e1a8a7eb356edadaf81d14edc9766a779eea5
Brandon <Brandon@mindframe.com>
2016-10-14 14:05:22 -0500

    added game loop

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,14 @@
+"use strict"
+
 const socket = io();
 
 socket.on('connection', function(){
   console.log(socket.id);
 });
 
+var fpsInterval, lastDrawTime, frameCount, lastSampleTime;
+var intervalID, requestID, then;
+var DELTA = .033;
 //Create the renderer
 const renderer = PIXI.autoDetectRenderer(256, 256, {
   //options
@@ -17,7 +22,7 @@ renderer.view.style.position = "absolute";
 renderer.view.style.display = "block";
 renderer.autoResize = true;
 renderer.resize(window.innerWidth, window.innerHeight);
-
+var keyboard = new THREEx.KeyboardState(renderer.domElement);
   //Create a container object called the `stage`
 const stage = new PIXI.Container();
 var sprite;
@@ -30,24 +35,54 @@ function setup() {
   sprite = new PIXI.Sprite(
     PIXI.loader.resources["img/explorer.png"].texture
   );
+  sprite.vx = 0;
+  sprite.vy = 0;
+  sprite.x = window.innerWidth / 2;
+  sprite.y = window.innerHeight / 2;
+
+  keyboard.domElement.addEventListener('keydown', function(event){
+    if (event.repeat) {
+      return;
+    }
+    if ( keyboard.eventMatches(event, 'A') || keyboard.eventMatches(event, 'left')){
+      sprite.vx -= 100 * DELTA;   
+    }
+    if ( keyboard.eventMatches(event, 'D') || keyboard.eventMatches(event, 'right')){
+      sprite.vx += 100 * DELTA;   
+    }
+    if ( keyboard.eventMatches(event, 'W') || keyboard.eventMatches(event, 'up')){
+      sprite.vy -= 100 * DELTA;   
+    }
+    if ( keyboard.eventMatches(event, 'S') || keyboard.eventMatches(event, 'down')){
+      sprite.vy += 100 * DELTA;   
+    }
+  })
+
+  keyboard.domElement.addEventListener('keyup', function(event){
+    if (event.repeat) {
+      return;
+    }
+    if ( keyboard.eventMatches(event, 'A') || keyboard.eventMatches(event, 'left')){
+      sprite.vx = 0;   
+    }
+    if ( keyboard.eventMatches(event, 'D') || keyboard.eventMatches(event, 'right')){
+      sprite.vx = 0;   
+    }
+    if ( keyboard.eventMatches(event, 'W') || keyboard.eventMatches(event, 'up')){
+      sprite.vy = 0;   
+    }
+    if ( keyboard.eventMatches(event, 'S') || keyboard.eventMatches(event, 'down')){
+      sprite.vy = 0;   
+    }
+  })
 
   //Add the cat to the stage
   stage.addChild(sprite);
 
-  Mousetrap.bind('w', function(){
-    sprite.y -= 5;
-  });
-  Mousetrap.bind('s', function(){
-    sprite.y += 5;
-  });
-  Mousetrap.bind('d', function(){
-    sprite.x += 5;
-  });
-  Mousetrap.bind('a', function(){
-    sprite.x -= 5;
-  });
+
   //Start the game loop
-  gameLoop();
+  then = Date.now();
+  startAnimating(60, 1000);
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
@@ -60,11 +95,52 @@ window.onresize = function(event) {
   renderer.resize(window.innerWidth, window.innerHeight);
 }
 
-function gameLoop() {
+function startAnimating(fps, sampleFreq) {
+    fpsInterval = 1000 / fps;
+    lastDrawTime = performance.now();
+    lastSampleTime = lastDrawTime;
+    frameCount = 0;
+    
+    animate();
+    
+    intervalID = setInterval(sampleFps, sampleFreq);
+}
 
-  //Loop this function at 60 frames per second
-  requestAnimationFrame(gameLoop);
+function animate(now) {
+    // request another frame
+    
+    requestID = requestAnimationFrame(animate);
 
-  //Render the stage to see the animation
-  renderer.render(stage);
+    // calc elapsed time since last loop
+    var elapsed = now - lastDrawTime;
+    
+    // if enough time has elapsed, draw the next frame
+    if (elapsed > fpsInterval) {
+
+        // Get ready for next frame by setting lastDrawTime=now, but...
+        // Also, adjust for fpsInterval not being multiple of 16.67
+        lastDrawTime = now - (elapsed % fpsInterval);
+
+        DELTA = elapsed / 1000;
+        console.log(DELTA);
+        sprite.x += sprite.vx;
+        sprite.y += sprite.vy
+        // draw
+        renderer.render(stage);
+
+        frameCount++;
+    }
+}
+
+function sampleFps() {
+    // sample FPS
+    var now = performance.now();
+    if (frameCount > 0) {
+        var currentFps =
+            (frameCount / (now - lastSampleTime) * 1000).toFixed(2);
+        
+        frameCount = 0;
+        
+    }
+    lastSampleTime = now;
 }
